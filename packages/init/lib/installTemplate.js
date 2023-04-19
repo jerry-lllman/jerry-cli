43c083e7004d38ede72260cf91a44f1271753f0f
jerry-lllman <jerry_lllman@163.com>
2023-04-19 22:18:41 +0800

    chore: 使用ejs实现基础版本的动态内容模版

--- packages/init/lib/installTemplate.js
+++ packages/init/lib/installTemplate.js
@@ -2,9 +2,10 @@ import path from 'node:path'
 
 import fse from 'fs-extra'
 import { pathExistsSync } from 'path-exists'
-import { log } from '@jerrytestgroup/utils'
+import { log, printErrorLog } from '@jerrytestgroup/utils'
 import ora from 'ora'
-
+import { glob } from 'glob'
+import ejs from 'ejs'
 
 function getCacheFilePath(targetPath, template) {
 	return path.resolve(targetPath, 'node_modules', template.npmName, 'template')
@@ -18,6 +19,27 @@ function copyFile(targetPath, template, installDir) {
 		fse.copySync(`${originDir}/${file}`, `${installDir}/${file}`)
 	})
 	spinner.succeed('模版拷贝成功')
+}
+
+async function ejsRender(installDir) {
+	try {
+		const files = await glob('**', { cwd: installDir, nodir: true, ignore: ["**/node_modules/**"] })
+		files.map(file => {
+			const filePath = path.join(installDir, file)
+			ejs.renderFile(filePath, {
+				data: {
+					name: 'react-template'
+				}
+			}).then(result => {
+				fse.writeFileSync(filePath, result)
+			}).catch(error => {
+				printErrorLog()(error)
+			})
+		})
+	} catch (error) {
+		printErrorLog()(error)
+	}
+
 }
 
 export default async function installTemplate(selectedTemplate, options) {
@@ -40,4 +62,6 @@ export default async function installTemplate(selectedTemplate, options) {
 		fse.ensureDirSync(installDir)
 	}
 	copyFile(targetPath, template, installDir)
+
+	ejsRender(installDir)
 }
